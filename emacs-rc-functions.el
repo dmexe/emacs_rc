88dd5040eba4abd955c4adf429fc6136d238e8de
Dmitry Galinsky <dima.exe@gmail.com>
2008-07-08 03:35:19 +0400

    Updated color-theme.

--- emacs-rc-functions.el
+++ emacs-rc-functions.el
@@ -21,7 +21,7 @@
     (save-excursion
       (let ((remove-count 0))
         (goto-char (point-min))
-        (while (re-search-forward "$" (point-max) t)
+        (while (re-search-forward "" (point-max) t)
           (setq remove-count (+ remove-count 1))
           (replace-match "" nil nil))
         (message (format "%d ^M removed from buffer." remove-count))))))
